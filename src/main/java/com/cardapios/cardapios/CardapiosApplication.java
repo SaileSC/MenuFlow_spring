91a54bf7d9fc75eb4aa2550f3ad5a23f4b4a8056
SaileSC <sailesantos0@gmail.com>
2024-09-21 23:29:02 -0400

    init project

---
+++ src/main/java/com/cardapios/cardapios/CardapiosApplication.java
@@ -0,0 +1,13 @@
+package com.cardapios.cardapios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardapios/cardapios/CardapiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardapios.cardapios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
